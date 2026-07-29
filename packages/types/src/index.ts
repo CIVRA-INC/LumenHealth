e853d2469e141f26b7477375730e30af7637a695
Daveamhs <ameh6736@student.babcock.edu.ng>
2026-07-29 18:20:56 +0100

    fix(index): improve index.ts functionality

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -4,6 +4,7 @@ export * from "./staff.js";
 export * from "./audit.js";
 export * from "./hashing.js";
 export * from "./merkle.js";
+export * from "./patient.js";
 
 export type PaymentIntent = {
   id: string;
